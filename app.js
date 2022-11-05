8869e9143642d024f9bf54fb292e0156366b6d2f
Noseble <manydreamcometure@gmail.com>
2022-11-05 23:04:51 +0800

    add handlebars

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 // require packages used in the project
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const restaurantList = require("./restaurant.json");
+const app = express();
+const port = 3000;
+
+// require handlebars in the project
+const exphbs = require("express-handlebars");
+
+// setting template engine
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// setting static files
+app.use(express.static("public"));
 
 // routes setting
-app.get('/', (req, res) => {
-  res.send('This is my restaurant List built with Express')
-})
+app.get("/", (req, res) => {
+  res.render("index", { restaurant: restaurantList.results });
+});
 
 // start and listen on the Express server
 app.listen(port, () => {
-  console.log(`Express is listening on localhost:${port}`)
-})
+  console.log(`Express is listening on localhost:${port}`);
+});
