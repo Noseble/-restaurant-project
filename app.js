b58d7a02d633876d4d50a52e58a77a4ece896323
Noseble <manydreamcometure@gmail.com>
2022-11-06 21:02:08 +0800

    add show page add search

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // require packages used in the project
 const express = require("express");
-const restaurantList = require("./restaurant.json");
+const restaurantList = require("./restaurant.json").results;
 const app = express();
 const port = 3000;
 
@@ -16,7 +16,21 @@ app.use(express.static("public"));
 
 // routes setting
 app.get("/", (req, res) => {
-  res.render("index", { restaurant: restaurantList.results });
+  res.render("index", { restaurant: restaurantList });
+});
+
+app.get("/search", (req, res) => {
+  const keywords = req.query.keyword
+  const keyword = keywords.toLowerCase().trim()
+  const restaurants = restaurantList.filter(data => {
+    return data.name.toLowerCase().trim().includes(keyword) || data.category.toLowerCase().trim().includes(keyword)
+  })
+  res.render("index", { restaurant: restaurants, keyword: keyword});
+});
+
+app.get("/restaurants/:restaurant_id", (req, res) => {
+  const restaurant = restaurantList.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
+  res.render("show", { restaurant: restaurant });
 });
 
 // start and listen on the Express server
